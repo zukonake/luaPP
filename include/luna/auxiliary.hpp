f5b419a208a819716d85c4e97d422eb3cea7d16b
zukonake <zeukmc@gmail.com>
2017-03-25 00:23:02 +0100

    Changed version to 0.0.0

--- include/luna/auxiliary.hpp
+++ include/luna/auxiliary.hpp
@@ -16,7 +16,7 @@
 namespace Luna::Auxiliary
 {
 
-constexpr uint8_t lunaVersionMajor = 1;
+constexpr uint8_t lunaVersionMajor = 0;
 constexpr uint8_t lunaVersionMinor = 0;
 constexpr uint8_t lunaVersionPatch = 0;
 
